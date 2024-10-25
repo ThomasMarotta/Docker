67029d77739c9f8ddd93d6c2cc92a08ccd73def4
Thomas Marotta <119932650+ThomasMarotta@users.noreply.github.com>
2024-10-25 13:56:25 +0200

    Update

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -22,19 +22,4 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:1969")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowCredentials(true);
-            }
-        };
-    }
 }
--- init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE hello(
-    id          SERIAL PRIMARY KEY,
-    messages    VARCHAR NOT NULL
-);
-
-INSERT INTO hello (messages) VALUES
-    ('Hello World');
